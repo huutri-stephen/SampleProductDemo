a26ae4f1b27d5c800e191ee4aa4ffc84c384b8bf
huutri-stephen <huutri.stephen@gmail.com>
2023-06-11 02:03:34 +0700

    update controller inser,update,delete products

--- src/main/java/org/example/ProductManager/controllers/ProductController.java
+++ src/main/java/org/example/ProductManager/controllers/ProductController.java
@@ -6,8 +6,12 @@ import org.example.ProductManager.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -39,4 +43,50 @@ public class ProductController {
 		);
 	}
 
+	@PostMapping("/insert")
+	ResponseEntity<ResponseObject> insertProduct(@RequestBody Product newProduct) {
+		List<Product> foundProduct = productRepository.findByProductName(newProduct.getProductName().trim());
+		if(foundProduct.size() > 0) {
+			return ResponseEntity.status(HttpStatus.NOT_IMPLEMENTED).body(
+					new ResponseObject("ERROR", "Product name already taken", "")
+			);
+		}
+
+		return ResponseEntity.status(HttpStatus.OK).body(
+				new ResponseObject("OK", "Insert product successfully", productRepository.save(newProduct))
+		);
+	}
+
+	@PutMapping("/{id}")
+	ResponseEntity<ResponseObject> insertProduct(@RequestBody Product newProduct, @PathVariable Long id) {
+		Product updateProduct = productRepository.findById(id)
+				.map(product -> {
+					product.setProductName(newProduct.getProductName());
+					product.setYear(newProduct.getYear());
+					product.setPrice(newProduct.getPrice());
+					product.setUrl(newProduct.getUrl());
+					return productRepository.save(product);
+				}).orElseGet(() -> {
+					newProduct.setId(id);
+					return productRepository.save(newProduct);
+				});
+		return ResponseEntity.status(HttpStatus.OK).body(
+				new ResponseObject("OK", "Update product successfully", updateProduct)
+		);
+	}
+
+	@DeleteMapping("/{id}")
+	ResponseEntity<ResponseObject> deleteProduct(@PathVariable Long id) {
+		boolean exists = productRepository.existsById(id);
+		if(!exists) {
+			productRepository.deleteById(id);
+			return ResponseEntity.status(HttpStatus.OK).body(
+					new ResponseObject("OK", "Delete product successfully", "")
+			);
+		} else  {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(
+					new ResponseObject("ERROR", "Delete product fail", "")
+			);
+		}
+	}
 }
--- src/main/java/org/example/ProductManager/repository/ProductRepository.java
+++ src/main/java/org/example/ProductManager/repository/ProductRepository.java
@@ -3,6 +3,8 @@ package org.example.ProductManager.repository;
 import org.example.ProductManager.models.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProductRepository extends JpaRepository<Product, Long> {
+import java.util.List;
 
+public interface ProductRepository extends JpaRepository<Product, Long> {
+	List<Product> findByProductName(String productName);
 }
